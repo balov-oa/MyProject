e545a2a8eaf3d13542012c1f43f46c7bc0696325
balov-oa <balov-oa@yandex.ru>
2020-09-20 01:47:08 +0700

    Initial commit

---
+++ run jupyter.bat
@@ -0,0 +1,4 @@
+cd C:\Users\qwerty.Oleg\Anaconda3\Scripts
+call activate.bat
+call cd C:\Users\qwerty.Oleg\Desktop\MyProject
+call jupyter notebook
---
+++ some.py
@@ -0,0 +1,12 @@
+def func():
+    return 1
+
+
+
+a = 1
+
+def func2():
+    return a + 2
+
+b = func2()
+print(a, b)
